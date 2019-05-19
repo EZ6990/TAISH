341a16bcdb690c3f1619611025098421c447c0d2
טל מלול <talmalu@auth.ad.bgu.ac.il>
2019-05-19 19:22:53 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var DButilsAzure = require('./DButils');
+
+var port = 3000;
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port);
+});
+
+app.get('/getALLPOI', function(req, res){
+    DButilsAzure.execQuery("SELECT * FROM PointsOfInterest")
+    .then(function(result){
+        res.send(result)
+    })
+    .catch(function(err){
+        console.log(err)
+        res.send(err)
+    })
+})
