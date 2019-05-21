5d71f0306b83a5a10a961bc94095f758bdbb416e
טל מלול <talmalu@auth.ad.bgu.ac.il>
2019-05-22 00:59:30 +0300

    Commited

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.get('/getSavedPOI', function(req, res){
     const user_data = {
         username: req.body.username,
     };
-        DButilsAzure.execQuery("SELECT poi.id,poi.Name,poi.Rate,poi.Description,poi.Picture FROM FavoritePointsOfInterest fpoi JOIN PointsOfInterest poi ON (poi.Id = fpoi.PointOfInterestId) Where fpoi.UserId = '" + user_data.username + "'")
+        DButilsAzure.execQuery("SELECT poi.id,poi.Name,poi.Rate,poi.Description,poi.Picture FROM FavoritePointsOfInterest fpoi JOIN PointsOfInterest poi ON (poi.Id = fpoi.PointOfInterestId) Where fpoi.UserId = '" + user_data.username + "' ORDER BY fpoi.Priorety ASC")
         .then(function(result){
                 let allpromisess = []
                 for (index = 0; index < result.length; index++) {
@@ -68,6 +68,98 @@ app.get('/getSavedPOI', function(req, res){
             res.send(err)
         })
 })
+
+app.get('/getReccomendedPOI', function(req, res){
+    const user_data = {
+        username: req.body.username,
+    };
+        DButilsAzure.execQuery("SELECT CategoryId FROM UsersCategories WHERE UserId = '" + user_data.username + "'")
+        .then(function(result){
+                let allpromisess = []
+                for (index = 0; index < result.length; index++) {
+                    let row = result[index];
+                    allpromisess.push(DButilsAzure.execQuery("SELECT poi.id,poi.Name,poi.Rate,poi.Description,poi.Picture "
+                                                            +"FROM PointsOfInterestCategories pc JOIN PointsOfInterest poi on (pc.PointOfIntrestId = poi.Id) "
+                                                            +"WHERE pc.CategoryId = " + row['CategoryId']
+                                                            +"AND poi.id NOT IN (SELECT PointOfInterestId FROM FavoritePointsOfInterest WHERE UserId = '" + user_data.username +"')"));
+
+                    allpromisess.push(DButilsAzure.execQuery("SELECT * FROM Categories WHERE Id = " + row['CategoryId']));
+                }
+                Promise.all(allpromisess).then(function(data)
+                {
+                    let RecommendedPointOfInterest = [];
+                    for (index = 0; index < data.length; index+=2) {
+                        let row = data[index][0];
+                        row['Categories'] = data[index+1];
+                        RecommendedPointOfInterest.push(row);
+                    }
+                    res.send(RecommendedPointOfInterest);
+                })
+        })
+        .catch(function(err){
+            console.log("error1 : " + err)
+            res.send(err)
+        })
+})
+
+app.post('/registerClient', function(req, res){
+    const user_data = {
+        username: req.body.username,
+        password: req.body.password,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        city: req.body.city,
+        country: req.body.country,
+        email: req.body.email,        
+        question: req.body.question,
+        answer: req.body.answer,
+        categories: req.body.categories
+    };
+    DButilsAzure.execQuery("INSERT INTO Users (Username,Password,Firstname,Lastname,City,Country,Email,SecurityQuestion,SecurityAnswer)"
+    +" VALUES ('"+user_data.username+"','"+user_data.password+"','"+user_data.firstname+"','"+user_data.lastname+"','"+user_data.city+"','"+user_data.country+"','"+user_data.email+"','"+user_data.question+"','"+user_data.answer+"')")
+    .then(function(result){
+        let categoriesPromises = []
+        for (index = 0; index < req.body.categories.length; index++) {
+            categoriesPromises.push(DButilsAzure.execQuery("INSERT INTO UsersCategories (UserId,CategoryId) VALUES ('"+user_data.username+"',"+user_data.categories[index]+")" ))
+        }        
+        Promise.all(categoriesPromises).then(function(data){
+            if (data.length != 0 && data[0].length != 0){
+                let delusercategories = DButilsAzure.execQuery("DELETE FROM UsersCategories WHERE UserId = '" +user_data.username+"'")
+                let deluser = DButilsAzure.execQuery("DELETE FROM Users WHERE Username = '" +user_data.username+"'")
+                Promise.all([delusercategories,deluser]).then(function(data){
+                    console.log("success delete Category")
+                    console.log("success delete user")
+                })
+                console.log("error1 : " + data)
+            }
+        })
+    })
+    .then(function(result){
+        res.status(401).send(result)
+    })
+    .catch(function(err){
+        console.log(err)
+        // DButilsAzure.execQuery("DELETE FROM UsersCategories WHERE UserId = '" +user_data.username+"'").then(function(data){
+        //     console.log("success delete user categories")
+        // })
+        // DButilsAzure.execQuery("DELETE FROM Users WHERE Username = '" +user_data.username+"'").then(function(data){
+        //     console.log("success delete user")
+        // })
+        res.send(err)
+    })
+})
+
+app.get('/getAllCategories', function(req, res){
+    DButilsAzure.execQuery("SELECT * FROM Categories")
+    .then(function(result){
+        res.status(200).send(result)
+    })
+    .catch(function(err){
+        console.log(err)
+        res.send(err)
+    })
+})
+
 app.put('/insertDefaultPOIOrder', function(req, res){
     // DButilsAzure.execQuery("SELECT PointOfInterestId , Priorety FROM FavoritePointsOfInterest WHERE UserId= "+req.body.username)
     // .then(function(result){
