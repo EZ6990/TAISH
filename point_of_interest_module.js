c4a125915dfd638282f9673ef7e5314b61ec6c65
Danny <dannykoyfman@gmail.com>
2019-05-29 12:44:57 +0300

    POIViewIncrease

--- point_of_interest_module.js
+++ point_of_interest_module.js
@@ -26,42 +26,62 @@ router.get('/getALLPOI', function (req, res) {
                     res.send(err)
                 })
         })
-    });
+});
 
+router.put('/POIViewIncrease', function(req,res){
+    DButilsAzure.execQuery("UPDATE PointsOfInterest SET point_viewed = point_viewed + 1 WHERE Id= " + req.body.POIId)
+    .then(function (result) {
+        res.sendStatus(200)
+    })
+    .catch(function (err) {
+        console.log(err)
+        res.send(err)
+    })
+})
 
-    router.get('/getPOI/:POIId', function (req, res) {
-        DButilsAzure.execQuery("SELECT * FROM PointsOfInterest WHERE Id = " + req.params.POIId)
-            .then(function (result) {
-                let allpromisess = []
-                for (index = 0; index < result.length; index++) {
-                    let row = result[index];
-                    allpromisess.push(row)
-                    allpromisess.push(DButilsAzure.execQuery("SELECT c.Id,c.Name,c.Description FROM PointsOfInterestCategories pc JOIN Categories c ON (pc.CategoryId = c.Id) WHERE pc.PointOfIntrestId = " + row['id']))
+router.get('/getPOI/:POIId', function (req, res) {
+    DButilsAzure.execQuery("SELECT * FROM PointsOfInterest WHERE Id = " + req.params.POIId)
+        .then(function (result) {
+            let allpromisess = []
+            for (index = 0; index < result.length; index++) {
+                let row = result[index];
+                allpromisess.push(row)
+                allpromisess.push(DButilsAzure.execQuery("SELECT c.Id,c.Name,c.Description FROM PointsOfInterestCategories pc JOIN Categories c ON (pc.CategoryId = c.Id) WHERE pc.PointOfIntrestId = " + row['id']))
+            }
+            Promise.all(allpromisess).then(function (data) {
+                let FavoritePointOfInterest = [];
+                for (index = 0; index < data.length; index += 2) {
+                    let row = data[index];
+                    row['Categories'] = data[index + 1];
+                    FavoritePointOfInterest.push(row)
                 }
-                Promise.all(allpromisess).then(function (data) {
-                    let FavoritePointOfInterest = [];
-                    for (index = 0; index < data.length; index += 2) {
-                        let row = data[index];
-                        row['Categories'] = data[index + 1];
-                        FavoritePointOfInterest.push(row)
-                    }
-                    res.send(FavoritePointOfInterest);
-                })
+                res.send(FavoritePointOfInterest);
             })
-                .catch(function (err) {
-                    console.log(err)
-                    res.send(err)
-                })
+        })
+        .catch(function (err) {
+            console.log(err)
+            res.send(err)
+        })
 
 });
-    router.get('/getPOIReviews/:POIId/:integer', function (req, res) {
-        let s = "";
-        if (req.params.integer == -1)
-            s = "SELECT * FROM Reviews WHERE PointOfInterestId= ";
-        else {
-            s = "SELECT * TOP " + req.params.integer + " FROM Reviews WHERE PointOfInterestId= ";
-        }
-        DButilsAzure.execQuery(s + req.params.POIId +" ORDER BY dateOfReview")
+router.get('/getPOIReviews/:POIId/:integer', function (req, res) {
+    let s = "";
+    if (req.params.integer == -1)
+        s = "SELECT * FROM Reviews WHERE PointOfInterestId= ";
+    else {
+        s = "SELECT * TOP " + req.params.integer + " FROM Reviews WHERE PointOfInterestId= ";
+    }
+    DButilsAzure.execQuery(s + req.params.POIId + " ORDER BY dateOfReview")
+        .then(function (result) {
+            res.status(200).send(result)
+        })
+        .catch(function (err) {
+            console.log(err)
+            res.send(err)
+        })
+
+    router.get('/getAllCategories', function (req, res) {
+        DButilsAzure.execQuery("SELECT * FROM Categories")
             .then(function (result) {
                 res.status(200).send(result)
             })
@@ -69,18 +89,8 @@ router.get('/getALLPOI', function (req, res) {
                 console.log(err)
                 res.send(err)
             })
+    })
 
-router.get('/getAllCategories', function(req, res){
-    DButilsAzure.execQuery("SELECT * FROM Categories")
-    .then(function(result){
-        res.status(200).send(result)
-    })
-    .catch(function(err){
-        console.log(err)
-        res.send(err)
-    })
-})
-
-module.exports = router;
-    })
     module.exports = router;
+})
+module.exports = router;
